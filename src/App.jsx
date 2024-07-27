f3bb3dcfd8c57c2f8532a20fb899d47c008fffe9
uniboxx <unibox@duck.com>
2024-07-27 08:59:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <Header>
+        <h1>ReactProject</h1>
+      </Header>
+      <Main>Main</Main>
+      <Footer>Footer</Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/react_usereducer_bank_account/',
+  root: 'src',
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
+  build: {
+    minify: true,
+    assetsInlineLimit: 0,
+    outDir: '../dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+  plugins: [react()],
+});
